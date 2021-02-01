30605c90a9b5d20ad5d4e2069bf82d3456fe9ef1
triggxl <triggxl@hotmail.com>
2021-02-01 17:32:54 -0500

    setting up database with expresss boilerplate; working on 1st migration

---
+++ articles_category.sql
@@ -0,0 +1,12 @@
+-- 2nd migration step
+CREATE TYPE article_category AS ENUM (
+  'Listicle',
+  'How-to',
+  'News',
+  'Interview',
+  'Story'
+)
+
+ALTER TABLE blogful_articles
+  ADD COLUMN
+    style article_category;
