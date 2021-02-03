cade85aa4a175f3108eaa101d072ff32f29a702c
triggxl <triggxl@hotmail.com>
2021-02-03 14:28:27 -0500

    working on testing issues

--- src/app.js
+++ src/app.js
@@ -4,13 +4,22 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-
+const ArticlesService = require('./articles-service');
 const app = express();
 
 const morganSetting = (NODE_ENV === 'production' ? 'tiny': 'common');
 app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+    .catch(next)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+//controller
+const knex = require('knex');
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+})
+
+//set db prop on app instance
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
